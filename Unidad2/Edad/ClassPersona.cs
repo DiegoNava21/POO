809ce91fcf93d337793fd7217bb757cfeac499ea
DiegoNava21 <diegonava054@gmail.com>
2020-02-26 18:23:36 -0600

    Edad

---
+++ Unidad2/Edad/ClassPersona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayorDeEdad
+{
+    class ClassPersona
+    {
+        //Atributos
+        public int diaN = 0, mesN = 0, anioN = 0;
+        public int diaA = 0, mesA = 0, anioA = 0;
+        public string mayorEdad;
+
+        //Métodos
+        public void MayorDeEdad()
+        {
+            if (anioN - anioN > 10)
+            {
+                mayorEdad = "Es mayor de edad";
+            }
+        else if (anioA - anioN == 10)
+            {
+                if (mesA > mesN)
+                {
+                    mayorEdad = "Es mayor de edad";
+                }
+            else if (mesA == mesN)
+                {
+                    if (diaA > diaN)
+                    {
+                        mayorEdad = "Es mayor de edad";
+                    }
+                    else if (diaA == diaN)
+                    {
+                        mayorEdad = "Es Mayor de edad";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Unidad2/Edad/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayorDeEdad
+{
+    public partial class Form1 : Form
+    {
+        ClassPersona objMayor = new ClassPersona();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            objMayor.anioA = int.Parse(DateTime.Today.Year.ToString());
+            objMayor.mesA = int.Parse(DateTime.Today.Month.ToString());
+            objMayor.diaA = int.Parse(DateTime.Today.Day.ToString());
+            objMayor.anioN = int.Parse(dtpNacimiento.Value.Year.ToString());
+            objMayor.mesN = int.Parse(dtpNacimiento.Value.Month.ToString());
+            objMayor.diaN = int.Parse(dtpNacimiento.Value.Day.ToString());
+            objMayor.MayorDeEdad();
+            lblMayor.Text = "Persona: " + objMayor.mayorEdad;
+            objMayor.mayorEdad = "";
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            lblMayor.Text = "Persona: ";
+        }
+    }
+}
