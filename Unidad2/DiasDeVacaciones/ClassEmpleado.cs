4639eb04af1c0447731be8697004e7e27c7a61d0
DiegoNava21 <diegonava054@gmail.com>
2020-02-26 18:22:09 -0600

    Vacaciones

---
+++ Unidad2/DiasDeVacaciones/ClassEmpleado.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiasVacaciones
+{
+    class ClassEmpleado
+    {
+        //Atributos
+        public int aniosTrabajo = 0;
+        public int DDV = 0;
+
+        //Métodos
+        public void Vacaciones()
+        {
+            if (aniosTrabajo>0 & aniosTrabajo<=5)
+            {
+                DDV = DDV + 5;
+            }
+            else if (aniosTrabajo>=6 & aniosTrabajo<=10)
+            {
+                DDV = DDV + 10;
+            }
+            else if (aniosTrabajo>=11 & aniosTrabajo<20)
+            {
+                DDV = 10 + ((aniosTrabajo - 10) * 1);
+            }
+            else if (aniosTrabajo>=21)
+            {
+                DDV = 10 + ((aniosTrabajo- 10) * 1);
+            }
+            if (DDV>45)
+            {
+                DDV = 45;
+            }
+         }
+     }
+}
